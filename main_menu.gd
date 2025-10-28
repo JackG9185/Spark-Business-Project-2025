9fd674454b65f4b05b3235d7abe3184116a1c465
belskyj1985 <belskyj1985@parkwayschools.net>
2025-10-28 13:02:06 -0500

    Added Main Menu

--- main_menu.gd
+++ main_menu.gd
@@ -3,3 +3,11 @@ extends Node2D
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://controls_menu.tscn")
