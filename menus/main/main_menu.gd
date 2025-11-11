d2a52246bf6e01d9efb398ef0d82290c42eaa53e
wieserc8592 <wieserc8592@parkwayschools.net>
2025-11-11 13:33:39 -0600

    Started Creating Glow Function

--- menus/main/main_menu.gd
+++ menus/main/main_menu.gd
@@ -11,3 +11,22 @@ func _on_quit_pressed() -> void:
 
 func _on_controls_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/controls/controls_menu.tscn")
+
+
+func _on_play_mouse_entered() -> void:
+	$play.modulate = Color.GOLD
+
+func _on_controls_mouse_entered() -> void:
+	$controls.modulate = Color.GOLD
+
+func _on_quit_mouse_entered() -> void:
+	$quit.modulate = Color.GOLD
+
+func _on_play_mouse_exited() -> void:
+	$play.modulate = Color.WHITE
+
+func _on_controls_mouse_exited() -> void:
+	$controls.modulate = Color.WHITE
+
+func _on_quit_mouse_exited() -> void:
+	$quit.modulate = Color.WHITE
