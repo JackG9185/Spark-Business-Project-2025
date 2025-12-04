479ab4bf0353af5e0b52be9fcbdc4e8bff0300ca
JackGoDev <jackgo0325@gmail.com>
2025-12-03 19:05:29 -0600

    level menu lerp on/off of screen

--- game.gd
+++ game.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @onready var pause_menu = $UI/pause_menu
-var paused = false
+@onready var level_up_menu: Control = $"UI/Level up menu"
 
+var paused = false
+var leveling = false
 func _ready() -> void:
 	pause_menu.hide()
 	Engine.time_scale = 1
@@ -10,6 +12,12 @@ func _ready() -> void:
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
+	if Input.is_action_just_pressed("dash"):
+		leveling = !leveling
+	if leveling:
+		level_up_menu.global_position.y = lerp(level_up_menu.global_position.y,-740.0,0.1) 
+	else:
+		level_up_menu.global_position.y = lerp(level_up_menu.global_position.y,0.0,0.1)
 
 func pauseMenu():
 	if paused:
